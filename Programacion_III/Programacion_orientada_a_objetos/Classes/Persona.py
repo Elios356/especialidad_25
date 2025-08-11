b6d9c05d00ed7c6a21ea5d339c3668925dda068c
Elios356 <tdgomez@escuelasproa.edu.ar>
2025-08-11 12:20:08 -0300

    Subiendo los archivos que faltaban

---
+++ Programacion_III/Programacion_orientada_a_objetos/Classes/Persona.py
@@ -0,0 +1,23 @@
+#Problema 1:
+#Implementaremos una clase llamada Persona que tendrá como atributo (variable) su
+#nombre y dos métodos (funciones), uno de dichos métodos inicializará el atributo
+#nombre y el siguiente método mostrará en la pantalla el contenidodel mi
+
+class Persona:
+
+    def inicializar(self,nom):
+        self.nombre=nom
+
+    def imprimir(self):
+        print("Nombre",self.nombre)
+
+
+# bloque principal
+
+persona1=Persona()
+persona1.inicializar("Pedro")
+persona1.imprimir()
+
+persona2=Persona()
+persona2.inicializar("Carla")
+persona2.imprimir()
